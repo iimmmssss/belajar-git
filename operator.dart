e5e1aef092c1009c87680414cb002258a1aa1709
Muhamad Ibrahim Fatah <muhamadibrahim219@gmail.com>
2024-09-21 00:13:14 +0700

    belajar git dengan menulis kode dart

---
+++ operator.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+    String nama;
+    String alamat;
+
+    nama = "Muhamad Ibrahim Fatah";
+    alamat = "Manggarai Jakarta";
+
+    print("Hello $nama dari $alamat");
+}
